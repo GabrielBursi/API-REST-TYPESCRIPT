e850c1c9e978870ac6d5dca66fabe7c9d0e28009
Gabriel Bursi <gabriellbursii@gmail.com>
2023-03-01 15:47:25 -0300

    TESTE DEPLOY 3

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import 'dotenv/config'
 import { Knex } from "./server/database/knex";
 
 const startServer = () => {
-    server.listen(process.env.PORT || 3333, () => {
+    server.listen(process.env.PORT || 3000, () => {
         console.log(`App rodando na porta ${process.env.PORT || 3333}`);
     });
 };
 
 if (process.env.IS_LOCALHOST !== 'true') {
     console.log('Rodando migrations');
-
+    
     Knex.migrate
         .latest()
         .then(() => {
