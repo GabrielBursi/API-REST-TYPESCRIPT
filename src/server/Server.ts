bb90db9c5819352f679d310214a30b5ec4c0badb
Gabriel Bursi <gabriellbursii@gmail.com>
2023-03-02 19:13:11 -0300

    config do cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors'
+
 import { router } from './routes';
 
 import './shared/services/Traducoes'
@@ -6,6 +8,9 @@ import './shared/services/Traducoes'
 const server = express();
 
 server.use(express.json())
+server.use(cors({
+    origin: process.env.ENABLE_CORS?.split(';') || []
+}))
 server.use(router)
 
 export default server
